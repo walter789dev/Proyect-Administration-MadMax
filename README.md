9423a44af07db451e980e205c25815d487d72d6e
Martyn Plaza <shuke656@gmail.com>
2024-10-21 15:28:18 -0300

    Actualizando readme.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# MadMax Administration
+
+Este proyecto permitirá gestionar recursos, procesos y usuarios de manera intuitiva, brindando a los administradores una interfaz moderna y amigable. El equipo planea aprovechar las ventajas de React, como su capacidad para crear interfaces de usuario rápidas y reutilizables, gracias a los componentes. Además, se integrarán API externas para manejar la base de datos y la autenticación de usuarios, garantizando seguridad y escalabilidad en la aplicación.
--- src/components/ui/containerGrid/ContainerGrid.tsx
+++ src/components/ui/containerGrid/ContainerGrid.tsx
@@ -1,16 +1,12 @@
-import { FC, ReactNode } from "react"
-import styles from './containerGrid.module.css'
+import { FC, ReactNode } from "react";
+import styles from "./containerGrid.module.css";
 
 interface GridProps {
-   children: ReactNode | ReactNode[]
+  children: ReactNode | ReactNode[];
 }
 
 const ContainerGrid: FC<GridProps> = ({ children }) => {
-   return (
-      <main className={styles.container}>
-      {children}
-    </main>
-  )
-}
+  return <main className={styles.container}>{children}</main>;
+};
 
-export default ContainerGrid
+export default ContainerGrid;
